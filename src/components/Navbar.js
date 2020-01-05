11090665c12ade72a9adccd06e4225e8722165dc
Estevao Fernandes <estevao.fernandes@live.fr>
2020-01-05 17:56:02 +0100

    navbar with style

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+      <nav className="Navbar" >https://fr.wikipedia.org/wiki/Les_Simpson
+      </nav>
+    );
+  }
+
+  ReactDOM.render(<Navbar />, document.getElementById('root'));
+
+  export default Navbar;
